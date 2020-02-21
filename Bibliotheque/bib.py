73e71261dbea4c6acad360ab42a7da00574a849e
HabibAroua <habibha.aroua82@gmail.com>
2020-02-21 14:37:19 +0100

    delete module

--- Bibliotheque/bib.py
+++ Bibliotheque/bib.py
@@ -4,8 +4,10 @@ class Document(models.Model):
      _name = 'biblioth.livre'
      name = fields.Char(string="Titre", required=True)
      description = fields.Text()
-     author_name = fields.Char(string='NomAuteur', required=True)
+     #author_name = fields.Char(string='NomAuteur', required=True)
+     author_name = fields.Many2many('res.partner', string='Auteurs')
      book_id = fields.Integer(string='ID dulivre', required=True)
      book_release_date = fields.Date(string="Date sortiedulivre")
      is_bestseller = fields.Boolean('Bestseller?')
      book_genre = fields.Selection([('h', 'Horreur'), ('s', 'Science'), ('a', 'Aventure'), ('r','Romance'), ('p', 'Policier'), ('au', 'Autre')],'Genre dulivre')
+     publisher_id = fields.Many2one('res.partner', string='Editeur', )
