751c24bfa5a482ef8f1be0c72442082ec50d3075
HabibAroua <habibha.aroua82@gmail.com>
2020-02-24 11:28:59 +0100

    add new fields

--- Exemple Heritage Bibliotheque/heritbib.py
+++ Exemple Heritage Bibliotheque/heritbib.py
@@ -3,3 +3,11 @@ from odoo import models, fields, api
 class DocumentHeritage(models.Model):
     _inherit = 'biblio.livre'
     pages_number = fields.Integer(string='Nombre de pages')
+
+class InscriptionBiblio(models.Model):
+     _name = 'biblioo.inscription'
+     nom_utilisateur = fields.Many2one('res.users', string='Utilisateur Bibliotheque', )
+     date_debut_inscription = fields.Date(string='Date inscription utilisateur')
+     date_fin_inscription = fields.Date(string='Date fin inscription utilisateur')
+     nombre_livres_lus = fields.Integer(string='Le nombre de livres lus par utilisateur')
+     
