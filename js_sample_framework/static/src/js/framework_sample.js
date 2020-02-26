77a1acdc7115ae4949da11e0bc65db14b1339daf
HabibAroua <habibha.aroua82@gmail.com>
2020-02-26 15:40:18 +0100

    get the code

--- js_sample_framework/static/src/js/framework_sample.js
+++ js_sample_framework/static/src/js/framework_sample.js
@@ -1,2 +1,8 @@
-alert('Loaded');
-console.log("loaded again");
+//alert('Loaded');
+//console.log("loaded again");
+
+odoo.define("js_sample_framework.SampleFrameWork" , function(require)
+{
+    'use strict';
+    var rpc = require('web.rpc');
+})
