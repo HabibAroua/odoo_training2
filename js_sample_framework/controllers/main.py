3b596b7b640be654bbf84c8c49d5430803b8119c
HabibAroua <habibha.aroua82@gmail.com>
2020-02-27 16:06:27 +0100

    show the template in the front

--- js_sample_framework/controllers/main.py
+++ js_sample_framework/controllers/main.py
@@ -7,6 +7,16 @@ class Doc(http.Controller):
     def funct(self,**kw):
         return "<h1>Hello world</h1>"
 
-    @http.route('/habib')
+    @http.route('/habib' , type="http")
     def funct(self, **kw):
         return "Holal Amigos"
+
+    @http.route('/test/path', type='http', methods=['GET'],csrf=False)
+    def test_path(self, **kw):
+        # here in kw you can get the inputted value
+        print (kw['name'])
+
+
+    @http.route('/custom/url' , website=True , auth='public')
+    def show_custom_webpage(self, **kw):
+        return request.render('js_sample_framework.new_web_page', {})
--- js_sample_framework/static/src/js/framework_sample.js
+++ js_sample_framework/static/src/js/framework_sample.js
@@ -1,8 +1,13 @@
 alert('Loaded');
 //console.log("loaded again");
-
+/*
 odoo.define("js_sample_framework.SampleFrameWork" , function(require)
 {
     'use strict';
     var rpc = require('web.rpc');
 })
+*/
+function click()
+{
+    alert("Hello World");
+}
